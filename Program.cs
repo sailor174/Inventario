ef584bd25f94ec8091443256b7eb702565bb7cae
unknown <samueldecandioscheffer@gmail.com>
2024-04-10 15:24:08 -0300

    bugfix(Program.cs): error decreasing potion ID

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks.Dataflow;
+﻿using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 using System.Timers;
 
 namespace Objetos
@@ -6,7 +8,6 @@ namespace Objetos
     public class Objeto()
     {
         static System.Timers.Timer timer;
-
         /*
         ideias:
         quando eu clicar Q/E ele vai diminuir/aumentar o Id da poção
@@ -26,15 +27,15 @@ namespace Objetos
             pocoes.Add(new Resist_potion("Resist freeze potion"));
             pocoes.Add(new Resist_potion("antidote"));
             int indiceAtual = 0;
-            Console.WriteLine("Oque deseja fazer?\n[1] sepuko \n[Q-E] poção\n[I] inventário\n[]");
+            Console.WriteLine("Oque deseja fazer?\n[1] sepuko \n[Q-E] poção\n[I] inventário\n[spacebar] usar item");
             while (true)
             {
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
                 switch (keyInfo.Key)
                 {
                     case ConsoleKey.Spacebar:
-                        Console.WriteLine("Você usou a " + );
+                        Console.WriteLine("Você usou a " + pocoes[indiceAtual].Name);
+                        pocoes[indiceAtual].Use();
                         break;
                     case ConsoleKey.D1:
                         Console.WriteLine("Quanto de vida deseja retirar?");
@@ -46,37 +47,38 @@ namespace Objetos
                             return;
                         }
                         break;
-
                     case ConsoleKey.E:
-                        indiceAtual = (indiceAtual + 1) % pocoes.Count;
+                        //verifica se é o ultimo ID
+                        if (indiceAtual == pocoes.Count - 1)
+                        {
+                            //volta do topo
+                            indiceAtual = -1;
+                        }
+                        indiceAtual++;
                         int tentativas = pocoes.Count;
-                        while (pocoes[indiceAtual].Qty >= 0 && tentativas >= 1)
-                        {
-                            indiceAtual = (indiceAtual + 1) % pocoes.Count;
-                        }
-                        if (tentativas == 0)
-                        {
-                            Console.WriteLine("sem mais poções");
-                        }
-                        Console.WriteLine($"Produto atual: {pocoes[indiceAtual].Name}, ID: {pocoes[indiceAtual].Id}");
+                        Console.WriteLine($"poção atual: {pocoes[indiceAtual].Name}, ID: {pocoes[indiceAtual].Id}");
                         break;
-
                     case ConsoleKey.Q:
-                        indiceAtual = (indiceAtual - 1) % pocoes.Count;
-                        Console.WriteLine($"Produto atual: {pocoes[indiceAtual].Name}, ID: {pocoes[indiceAtual].Id}");
+                        //verifica se é o ultimo ID
+                        if (indiceAtual == 0)
+                        {
+                            //volta do topo
+                            indiceAtual = pocoes.Count;
+                        }
+                        indiceAtual--;
+                        Console.WriteLine($"poção atual: {pocoes[indiceAtual].Name}, ID: {pocoes[indiceAtual].Id}");
                         break;
 
                     case ConsoleKey.I:
+
                         break;
                 }
             }
         }
-
         public class Player
         {
             public static int Vida { get; set; } = 500;
             public static int Vida_Max = 500;
-
             public Player()
             {
                 Vida = 500;
@@ -92,14 +94,17 @@ namespace Objetos
             private static int LastId { get; set; } = 0;
             protected int Max_qty { get; set; }
             public int Id { get; private set; }
-            public string Name { get; set; }
+            public virtual string Name { get; set; }
             public int Qty { get; private set; }
-
-            public Potion(int qty = 0)
+            public Potion(int qty = 1)
             {
                 LastId++;
                 Id = LastId;
                 Qty = qty;
+            }
+            public virtual void Use()
+            {
+
             }
         }
         public class Cure_potion : Potion
@@ -107,52 +112,44 @@ namespace Objetos
             public int Strenght { get; set; }
             public Cure_potion(string name, int strenght = -1)
             {
-                int Strenght = strenght;
-                string Name = name;
+                Strenght = strenght;
+                Name = name;
             }
-            ///*
             public void Use()
             {
                 if (Strenght == -1)
                 {
                     Player.Vida = Player.Vida_Max;
+                    Console.WriteLine("Você usou a poção de cura total");
                 }
                 else
                 {
-                    Player.Vida = Strenght;
+                    Player.Vida += Strenght;
                 }
             }
-            //*/
         }
-
         public class Resist_potion : Potion
         {
             public Resist_potion(string name)
             {
                 Name = name;
-
-                void Use()
+            }
+            public void Use()
+            {
+                switch (Name)
                 {
-                    switch (Name)
-                    {
-                        /*
-                        case "Resist fire potion":
-                            int fireTimer = 90000;
-
-                            break;
-                        case "antidote":
-                            int venomTimer = 90000;
-                            break;
-                        case "Resist shock potion":
-                            int shockTimer = 90000;
-
-                            break;
-                        case "Resiste freeze potion":
-                            int freezeTimer = 90000;
-
-                            break;
-                            */
-                    }
+                    case "Resist fire potion":
+                        int fireTimer = 90000;
+                        break;
+                    case "antidote":
+                        int venomTimer = 90000;
+                        break;
+                    case "Resist shock potion":
+                        int shockTimer = 90000;
+                        break;
+                    case "Resiste freeze potion":
+                        int freezeTimer = 90000;
+                        break;
                 }
             }
         }
