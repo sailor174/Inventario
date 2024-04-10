bf5541306946f0cccaa1bf279a762ade69475758
unknown <samueldecandioscheffer@gmail.com>
2024-04-10 19:56:26 -0300

    revert commit test

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection.Metadata;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks.Dataflow;
-using System.Timers;
+using System.Timers;    
 
 namespace Objetos
 {
@@ -19,13 +19,15 @@ namespace Objetos
         public static void Main(string[] args)
         {
             Player player = new();
-            List<Potion> pocoes = [];
-            pocoes.Add(new Cure_potion("Health potion", 150));
-            pocoes.Add(new Cure_potion("Full health potion"));
-            pocoes.Add(new Resist_potion("Resist fire potion"));
-            pocoes.Add(new Resist_potion("Resist shock potion"));
-            pocoes.Add(new Resist_potion("Resist freeze potion"));
-            pocoes.Add(new Resist_potion("antidote"));
+            List<Item> items = [];
+            
+            items.Add(new Cure_potion("Health potion", 150));
+            items.Add(new Cure_potion("Full health potion"));
+            items.Add(new Resist_potion("Resist fire potion"));
+            items.Add(new Resist_potion("Resist shock potion"));
+            items.Add(new Resist_potion("Resist freeze potion"));
+            items.Add(new Resist_potion("antidote"));
+
             int indiceAtual = 0;
             Console.WriteLine("Oque deseja fazer?\n[1] sepuko \n[Q-E] poção\n[I] inventário\n[spacebar] usar item");
             while (true)
@@ -34,8 +36,8 @@ namespace Objetos
                 switch (keyInfo.Key)
                 {
                     case ConsoleKey.Spacebar:
-                        Console.WriteLine("Você usou a " + pocoes[indiceAtual].Name);
-                        pocoes[indiceAtual].Use();
+                        Console.WriteLine("Você usou a " + items[indiceAtual].Name);
+                        items[indiceAtual].Use();
                         break;
                     case ConsoleKey.D1:
                         Console.WriteLine("Quanto de vida deseja retirar?");
@@ -49,30 +51,39 @@ namespace Objetos
                         break;
                     case ConsoleKey.E:
                         //verifica se é o ultimo ID
-                        if (indiceAtual == pocoes.Count - 1)
+                        if (indiceAtual == items.Count - 1)
                         {
                             //volta do topo
                             indiceAtual = -1;
                         }
                         indiceAtual++;
-                        int tentativas = pocoes.Count;
-                        Console.WriteLine($"poção atual: {pocoes[indiceAtual].Name}, ID: {pocoes[indiceAtual].Id}");
+                        int tentativas = items.Count;
+                        Console.WriteLine($"poção atual: {items[indiceAtual].Name}, ID: {items[indiceAtual].Id}");
                         break;
                     case ConsoleKey.Q:
                         //verifica se é o ultimo ID
                         if (indiceAtual == 0)
                         {
                             //volta do topo
-                            indiceAtual = pocoes.Count;
+                            indiceAtual = items.Count;
                         }
                         indiceAtual--;
-                        Console.WriteLine($"poção atual: {pocoes[indiceAtual].Name}, ID: {pocoes[indiceAtual].Id}");
+                        Console.WriteLine($"poção atual: {items[indiceAtual].Name}, ID: {items[indiceAtual].Id}");
                         break;
 
                     case ConsoleKey.I:
-
+                        Inventory.DisplayInventory();
                         break;
                 }
+            }
+        }
+
+        public class Inventory
+        {
+            int capacity = 15;
+            public void AddItem()
+            {
+                foreach 
             }
         }
         public class Player
@@ -89,25 +100,32 @@ namespace Objetos
                 Vida -= int.Parse(Console.ReadLine());
             }
         }
-        public class Potion
-        {
-            private static int LastId { get; set; } = 0;
+
+        public class Item
+        { 
+            public int Qty { get; set; }
+            protected static int LastId { get; set; } = 0;
             protected int Max_qty { get; set; }
-            public int Id { get; private set; }
             public virtual string Name { get; set; }
-            public int Qty { get; private set; }
-            public Potion(int qty = 1)
+            public int Id { get; set; }
+            public Item(int qty = 1)
             {
                 LastId++;
                 Id = LastId;
                 Qty = qty;
+                if(Qty > Max_qty)
+                {
+                    Qty = Max_qty;
+                    Console.WriteLine("Limite de poções atingido");
+                }
+                if (Item)
             }
             public virtual void Use()
             {
 
             }
         }
-        public class Cure_potion : Potion
+        public class Cure_potion : Item
         {
             public int Strenght { get; set; }
             public Cure_potion(string name, int strenght = -1)
@@ -121,14 +139,15 @@ namespace Objetos
                 {
                     Player.Vida = Player.Vida_Max;
                     Console.WriteLine("Você usou a poção de cura total");
+                    Qty --;
                 }
                 else
                 {
-                    Player.Vida += Strenght;
+                    Player.Vida += Strenght;      
                 }
             }
         }
-        public class Resist_potion : Potion
+        public class Resist_potion : Item
         {
             public Resist_potion(string name)
             {
@@ -147,7 +166,7 @@ namespace Objetos
                     case "Resist shock potion":
                         int shockTimer = 90000;
                         break;
-                    case "Resiste freeze potion":
+                    case "Resist freeze potion":
                         int freezeTimer = 90000;
                         break;
                 }
